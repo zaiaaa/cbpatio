a5d24f8ac987b406d7de574dc05419bbb19d7312
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2023-12-19 11:41:22 -0300

    Adicionando metodo getByName e tratamento da promise diretamente no controller

--- controller/campeonatoController.js
+++ controller/campeonatoController.js
@@ -1,8 +1,21 @@
 const campeonatoModel = require('../model/campeonatoModel')
 
-class CampeonatoController{
-    get(){
-        return campeonatoModel.get()
+class CampeonatoController {
+    get(req, res) {
+        const listagemDeCampeonatos = campeonatoModel.get()
+
+        listagemDeCampeonatos
+            .then(campeonatos => res.status(200).json(campeonatos))
+            .catch(err => res.status(400).json(err.message))
+    }
+
+    getByName(req, res, nome) {
+        const listagemDeCampeonatosPeloNome = campeonatoModel.getByName(nome)
+
+        listagemDeCampeonatosPeloNome
+            .then(campeonatos => res.status(200).json(campeonatos))
+            .catch(err => res.status(400).json(err.message))
+
     }
 }
 
