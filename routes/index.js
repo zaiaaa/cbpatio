a356d9afc6fcf10c02c2f85dbae3835d828edead
zaiaaa <gustavo.0801214@gmail.com>
2023-12-20 17:17:13 -0300

    ligando tudo no index das rotas e podendo utilizar-los

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const rotasCampeonatos = require('./campeonatoRotas')
+const rotasTimes = require('./timeRotas')
+
 
 module.exports = (app, express) => {
     app.use(express.json())
     app.use(express.urlencoded({extended: true}))
     app.use(rotasCampeonatos)
+    app.use(rotasTimes)
 }
