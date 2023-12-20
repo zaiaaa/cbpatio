91fe5ab45b613f8ce8a29a0717d0e1a2bb1b3478
zaiaaa <gustavo.0801214@gmail.com>
2023-12-20 17:16:28 -0300

    model de CRUD para times feito. Tem o mesmo tratamento do model de campeonatos

---
+++ model/timeModel.js
@@ -0,0 +1,43 @@
+const conn = require('../inc/conexao')
+
+class TimeModel{
+    executarQuery(sql, parametros=""){
+        return new Promise((res, rej) => {
+            conn.query(sql, parametros, (err, resp) => {
+                if(err){
+                    console.log('erro -> ', e)
+                    rej(e)
+                }
+                res(resp)
+            })
+        })
+    }
+
+    get(){
+        const sql = "SELECT * FROM time"
+        return this.executarQuery(sql)
+    }
+
+    getByName(nome){
+        const sql = "SELECT * FROM time WHERE nome LIKE ?"
+        return this.executarQuery(sql, `%${nome}%`)
+    }
+
+    novoTime(time){
+        const sql = "INSERT INTO time SET ?"
+        return this.executarQuery(sql, time)
+    }
+
+    alterarTime(time, id){
+        const sql = "UPDATE time SET ? WHERE id_time = ?"
+        return this.executarQuery(sql, [time, id])
+    }
+
+    deletarTime(id){
+        const sql = "DELETE FROM time WHERE id_time = ?"
+        return this.executarQuery(sql, id)
+    }
+}
+
+
+module.exports = new TimeModel
