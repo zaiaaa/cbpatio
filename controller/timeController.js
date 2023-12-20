b5b64070d60be8a67bdd1faff1afa16a25c91df5
zaiaaa <gustavo.0801214@gmail.com>
2023-12-20 17:16:37 -0300

    controller para times feito

---
+++ controller/timeController.js
@@ -0,0 +1,45 @@
+const timeModel = require('../model/timeModel')
+
+class timeController{
+    get(req, res){
+        const listaDeTimes = timeModel.get()
+
+        listaDeTimes.then(times => res.status(200).json(times))
+        .catch(err => res.status(400).json(err.message))
+    }
+
+    getByName(req, res){
+        const {nome} = req.params
+
+        const listaDeTimes = timeModel.getByName(nome)
+
+        listaDeTimes.then(times => res.status(200).json(times))
+        .catch(err => res.status(400).json(err.message))
+    }
+
+    novoTime(req, res){
+        const novoTime = req.body
+
+        timeModel.novoTime(novoTime)
+        .then(timeCadastrado => (res.status(201).json(timeCadastrado)))
+        .catch((err) => (res.status(400).json(err)))
+    }
+
+    alterarTime(req, res){
+        const alteracaoTime = req.body
+        const {id} = req.params
+
+        timeModel.alterarTime(alteracaoTime, id)
+        .then(timeAlterado => (res.status(201).json(timeAlterado)))
+        .catch((err) => (res.status(400).json(err)))
+    }
+
+    deletarTime(req, res){
+        const { id } = req.params
+
+        timeModel.deletarTime(id).then(timeDeletado => (res.status(204).json(timeDeletado)))
+        .catch((err) => (res.status(400).json(err)))
+    }
+}
+
+module.exports = new timeController()
