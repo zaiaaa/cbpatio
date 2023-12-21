3db8291690f01d076e80c0c90ff55cfd4ce21d70
zaiaaa <gustavo.0801214@gmail.com>
2023-12-20 22:23:09 -0300

    inclusao de usuario com senha encriptada OK

--- model/usuariosModel.js
+++ model/usuariosModel.js
@@ -1,4 +1,9 @@
 const conn = require('../inc/conexao')
+const bcrypt = require('bcrypt')
+
+const saltRound = 10;
+const initHash = 'iusehbghu923h4g23hnfas0ijd1h2ju1120';
+
 
 class UsuariosModel{
     executarQuery(sql, parametros=""){
@@ -17,6 +22,19 @@ class UsuariosModel{
         const sql = "SELECT * FROM usuario"
         return this.executarQuery(sql)
     }
+
+    async novoUsuario(usuario){
+        const sql = "INSERT INTO usuario SET ?"        
+        const {senha} = usuario
+        
+        const salt = await bcrypt.genSalt(saltRound);
+        const hash = await bcrypt.hash(senha, salt);
+
+        usuario.senha = hash
+
+        return this.executarQuery(sql, usuario)
+    }
 }
+
 
 module.exports = new UsuariosModel
