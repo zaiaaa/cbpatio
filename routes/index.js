4a9f29d08415ef0625e2da732c5410d0b148322e
zaiaaa <gustavo.0801214@gmail.com>
2023-12-20 21:36:58 -0300

    adicionando a rota usuarios no index das rotas

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const rotasCampeonatos = require('./campeonatoRotas')
 const rotasTimes = require('./timeRotas')
-
+const rotasUsuarios = require('./usuarioRotas')
 
 module.exports = (app, express) => {
     app.use(express.json())
     app.use(express.urlencoded({extended: true}))
     app.use(rotasCampeonatos)
     app.use(rotasTimes)
+    app.use(rotasUsuarios)
 }
