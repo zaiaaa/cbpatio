7ac6737722052927fa4471671f0c85e70d29ab61
zaiaaa <gustavo.0801214@gmail.com>
2023-12-20 22:47:47 -0300

    controller de login de usuario

--- controller/usuariosController.js
+++ controller/usuariosController.js
@@ -7,6 +7,16 @@ class UsuariosController{
         .catch(err => res.status(400).json(err.message))
     }
 
+    logUsuario(req, res){
+        const email = req.query.email
+        const senha = req.query.senha
+
+
+        usuariosModel.logUsuario(email, senha)
+        .then(usuario => res.status(200).json(usuario))
+        .catch(err => res.status(400).json(err.message))
+    }
+
     novoUsuario(req, res){
         const usuario = req.body
 
