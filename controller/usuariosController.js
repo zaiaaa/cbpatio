8d0952998c5a829d60756c08944ed7fa1d884cb3
zaiaaa <gustavo.0801214@gmail.com>
2023-12-20 22:23:24 -0300

    controller pra rota de novo usuario

--- controller/usuariosController.js
+++ controller/usuariosController.js
@@ -6,6 +6,14 @@ class UsuariosController{
         .then(users => res.status(200).json(users))
         .catch(err => res.status(400).json(err.message))
     }
+
+    novoUsuario(req, res){
+        const usuario = req.body
+
+        usuariosModel.novoUsuario(usuario)
+        .then(newUser => res.status(201).json(newUser))
+        .catch(err => res.status(400).json(err.message))
+    }
 }
 
 module.exports = new UsuariosController()
