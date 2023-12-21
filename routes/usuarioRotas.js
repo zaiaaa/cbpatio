43fcbd0988e80571ee7002b107d05d02e4d7f913
zaiaaa <gustavo.0801214@gmail.com>
2023-12-20 22:48:09 -0300

    rota e instruções de como fazer a requisição de login de usuarios

--- routes/usuarioRotas.js
+++ routes/usuarioRotas.js
@@ -1,7 +1,6 @@
 const usuariosController = require('../controller/usuariosController')
 
 const {Router} = require('express')
-const { route } = require('./timeRotas')
 const router = Router()
 
 router.get('/usuarios', (req, res) => {
@@ -10,6 +9,12 @@ router.get('/usuarios', (req, res) => {
 
 router.post('/usuarios/cadastrar', (req, res) => {
     usuariosController.novoUsuario(req, res)
+})
+
+router.get('/usuarios/login', (req, res) => {
+    //Requisição deve ser feita com /usuarios/login?email=email.com&senha=senha
+    //na parte de login do usuario, não é necessário o uso de jwt, mas para a visualização da api sim.
+    usuariosController.logUsuario(req, res)
 })
 
 module.exports = router
