5110b1b23330ee4e280b8719ca9e6bdbe51474ad
zaiaaa <gustavo.0801214@gmail.com>
2023-12-20 22:23:36 -0300

    rota pra inclusao de usuario

--- routes/usuarioRotas.js
+++ routes/usuarioRotas.js
@@ -1,10 +1,15 @@
 const usuariosController = require('../controller/usuariosController')
 
 const {Router} = require('express')
+const { route } = require('./timeRotas')
 const router = Router()
 
 router.get('/usuarios', (req, res) => {
     usuariosController.get(req, res)
+})
+
+router.post('/usuarios/cadastrar', (req, res) => {
+    usuariosController.novoUsuario(req, res)
 })
 
 module.exports = router
