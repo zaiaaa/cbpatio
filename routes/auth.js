3bccfdf36c2294c68e0752236624fd6bfcf7fcf1
zaiaaa <gustavo.0801214@gmail.com>
2023-12-24 18:31:54 -0300

    mudança de user para usuario para respoeitar os padroes do projeto

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ const { Router } = require('express')
 const router = Router();
 
 router.post('/login', (req, res) => {
-    const {user, senha} = req.body;
-    const auth = authenticate.Auth(user, senha)
+    const {usuario, senha} = req.body;
+    const auth = authenticate.Auth(usuario, senha)
     auth
     .then(login => res.status(201).json(login))
     .catch(err => res.status(400).json(err))
