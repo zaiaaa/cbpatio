ffe02384591b9e741007405a8da2e92d47131e7b
zaiaaa <gustavo.0801214@gmail.com>
2023-12-24 18:10:42 -0300

    mddleware para a intermediação das rotas protegidas toda vez que entrarmos na api, temos que ter o bearer token para autenticacao e uso da api

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization
+
+    if(!token){
+        return res.status(401).send({message: "Token obrigatório"})
+    }
+
+    try {
+        const replace = token.replace("Bearer ", "")
+        jwt.verify(replace, process.env.SENHA)
+        next()
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+module.exports = verifyToken
