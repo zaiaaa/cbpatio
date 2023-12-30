94728b88ee786041a4db5a3aa7ce92ff02eccc1b
zaiaaa <gustavo.0801214@gmail.com>
2023-12-29 21:04:38 -0300

    model da relação de time e usuario feito Todo o crud está feito (menos a edição)

--- model/modelTime_usuario.js
+++ model/modelTime_usuario.js
@@ -1,6 +1,6 @@
 const conn = require('../inc/conexao')
 
-class time_usuario{
+class Time_usuario{
     executarQuery(sql, parametros=""){
         return new Promise((resolve, rejects) => {
             conn.query(sql, parametros, (err, resp) => {
@@ -15,9 +15,29 @@ class time_usuario{
     }
 
     get(){
-        const sql = "SELECT * FROM time_usuario"
-        this.executarQuery(sql)
+        const sql = `SELECT tu.id_time_usuario as time_usuarioReg, t.id_time as idTime, t.nome AS NomeTime, u.id_usuario as idUser, u.nome_usuario AS NomeUsuario
+        FROM time_usuario tu
+        INNER JOIN time t ON tu.fk_id_time = t.id_time
+        INNER JOIN usuario u ON tu.fk_id_usuario = u.id_usuario;`
+        return this.executarQuery(sql)
     }
+
+    novoUsuarioNoTime(newUserTeam){
+        const sql = `INSERT INTO time_usuario SET ?`
+        return this.executarQuery(sql, newUserTeam)
+    }
+
+    async deletarUsuario_time(id_time, id_user){
+        const sql1 = "SELECT id_time_usuario FROM time_usuario WHERE fk_id_time = ? AND fk_id_usuario = ?"
+        console.log(id_time, id_user)
+        const jogador = await this.executarQuery(sql1, [id_time, id_user])
+
+        const jogadorBanido = jogador[0].id_time_usuario
+        const sql = `DELETE FROM time_usuario WHERE id_time_usuario = ?`
+        return this.executarQuery(sql, jogadorBanido)
+    }
+
+
 }   
 
-module.exports = new time_usuario
+module.exports = new Time_usuario
