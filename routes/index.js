e5727bc41519cb163d7b5cde1956d2511d3e1002
zaiaaa <gustavo.0801214@gmail.com>
2023-12-29 21:04:49 -0300

    jogando tudo para o index de rotas

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const rotasCampeonatos = require('./campeonatoRotas')
 const rotasTimes = require('./timeRotas')
 const rotasUsuarios = require('./usuarioRotas')
 const authRota = require('./auth')
+const time_usuariosRota = require('./time_usuarioRotas')
 
 module.exports = (app, express) => {
     app.use(express.json())
@@ -10,4 +11,5 @@ module.exports = (app, express) => {
     app.use(rotasTimes)
     app.use(rotasUsuarios)
     app.use(authRota)
+    app.use(time_usuariosRota)
 }
