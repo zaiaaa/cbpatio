1b13f06d1a5bb6c91f6bd023b8c1bfb235dbbf4d
zaiaaa <gustavo.0801214@gmail.com>
2024-01-01 23:00:27 -0300

    adicionando a rota ao index

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const rotasTimes = require('./timeRotas')
 const rotasUsuarios = require('./usuarioRotas')
 const authRota = require('./auth')
 const time_usuariosRota = require('./time_usuarioRotas')
+const time_campeonatosRota = require('./time_campeonatoRotas')
 
 module.exports = (app, express) => {
     app.use(express.json())
@@ -12,4 +13,5 @@ module.exports = (app, express) => {
     app.use(rotasUsuarios)
     app.use(authRota)
     app.use(time_usuariosRota)
+    app.use(time_campeonatosRota)
 }
