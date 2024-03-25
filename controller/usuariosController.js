c0faad7d88e823d3a8f3096e797d61e735e19076
zaiaaa <gustavo.0801214@gmail.com>
2024-03-25 20:47:47 -0300

    alterações p login

--- controller/usuariosController.js
+++ controller/usuariosController.js
@@ -22,7 +22,7 @@ class UsuariosController{
 
         usuariosModel.logUsuario(email, senha)
         .then(usuario => res.status(200).json(usuario))
-        .catch(err => res.status(400).json(err.message))
+        .catch(err => res.status(401).json(err.message))
     }
 
     novoUsuario(req, res){
