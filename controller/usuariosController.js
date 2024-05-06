d8349a8a41fdb4be82205405eca23f721af53b11
Pedro Castelo Branco dos Santos <113044972+CastleWhite23@users.noreply.github.com>
2024-05-06 08:24:59 -0300

    fds usuariosController.js

--- controller/usuariosController.js
+++ controller/usuariosController.js
@@ -52,7 +52,7 @@ class UsuariosController{
         
         !foto?.path ? "" : usuario.foto = foto?.path
         
-        if(foto?.path){
+        if(foto?.path && body.foto != "" && userJson[0]?.foto){
             const userFoto = userJson[0].foto
             const path = userFoto.replace(/\\/g, "/")
             
